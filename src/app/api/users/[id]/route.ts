cd411a447d4d20cbfe94f74f1757f4f2d52b4716
d3bn4th <arihantd2609@gmail.com>
2025-04-01 23:19:11 +0530

    route.ts file error

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/auth.options';
+import { authOptions } from '@/lib/auth-options';
 import prisma from '@/lib/prisma';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
